1b54277d8138dcb84005c4a9c732159f9292af29
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-22 22:04:16 +0200

    alte variabile publice in clasa human

--- tutoriale/tutorial.cpp
+++ tutoriale/tutorial.cpp
@@ -8,6 +8,8 @@ class HumanBeing
         {
             cout << "hello i am a human being";
         }
+        int nr = 5;
+        string nume = "ionut";
 };
 
 
@@ -15,6 +17,10 @@ int main()
 {
     HumanBeing humanBeing;
     humanBeing.display();
+    cout << endl;
+    cout << "numele: " << humanBeing.nume << endl;
+    cout << "varsta: " <<humanBeing.nr << endl;
+    
 
 
     return 0;
