5309a5875d7074a7f45f5f18f3e8b940baf882b7
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2021-11-28 15:09:00 +0200

    commit

--- laboratoare/laborator2/exercitiul1/Main.cpp
+++ laboratoare/laborator2/exercitiul1/Main.cpp
@@ -7,10 +7,18 @@ using namespace std;
 int main()
 {
 
-    Fractie fractie(3,4);
-    fractie.print();    
-    
+    Fractie fractie1(3,4);
+    Fractie fractie2(3,5);
+    cout << "fractie1: " << endl; 
+    fractie1.print();    
+    cout << "fractie2: " << endl;
+    fractie2.print();
 
+    fractie1.Adunare(fractie1,fractie2);
+    cout << endl;
+    fractie2.Scadere(fractie1,fractie2);
+    
+    cout << "CMMDC a celor doua numere este: " << fractie1.CMMDC(29,12);
     
     
 
